87b5c77f0314b42b658b24981e77c5191cda03a1
brendan99 <brendanacoen@gmail.com>
2020-05-04 10:05:30 +0100

    Angular Material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const routes: Routes = [
+  {
+    path: 'meds',
+    loadChildren: () =>
+      import('src/app/meds/meds-feature.module').then(
+        (m) => m.MedsFeatureModule
+      ),
+  },
+  {
+    path: 'growth',
+    loadChildren: './growth/growth-app.module#GrowthFeatureModule',
+  },
+  { path: '**', redirectTo: 'meds' },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
